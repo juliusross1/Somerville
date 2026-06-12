5148523b5f9da5ab8aedbe10c12e5c68dce927fd
Julius Ross <juliusross@Juliuss-MacBook-Air-2023.local>
2026-06-12 12:51:43 -0500

    Initial commit

---
+++ sources/makePlayfairVFs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This script generates the Variable Font TTFs for the Playfair typeface.
+
+# Checks if the `variable_ttf` directory exists, if not it creates it.
+if [ ! -d "variable_ttf" ]; then
+  mkdir -p "variable_ttf"
+  chmod -R 755 "variable_ttf"
+fi
+
+fontmake Playfair-2_2-Roman.glyphspackage -o variable \
+  --output-path 'variable_ttf/PlayfairRomanVF.ttf' --verbose WARNING
+fontmake Playfair-2_2-Italic.glyphspackage -o variable \
+  --output-path 'variable_ttf/PlayfairItalicVF.ttf' --verbose WARNING
+
+# EOF
